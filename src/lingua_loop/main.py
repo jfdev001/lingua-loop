7c7eb974e07b3f3d7ed46a18e5e4e4cc32336ba7
Jared Frazier <frazier@iap-kborn.de>
2026-03-06 20:45:36 +0100

    hello world works, uvicorn fails... check paths

--- src/lingua_loop/main.py
+++ src/lingua_loop/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from lingua-loop!")
+import uvicorn
+
+
+def run():
+    uvicorn.run(
+        "lingua_loop.app:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=False
+    )
 
 
 if __name__ == "__main__":
-    main()
+    run()
