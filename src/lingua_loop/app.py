87706863710d661ffe4972bec559c6af671bdba7
Jared Frazier <frazier@iap-kborn.de>
2026-03-07 08:59:04 +0100

    add db deps, static + templates, serve index.html, simple js

--- src/lingua_loop/app.py
+++ src/lingua_loop/app.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "static"
+TEMPLATES_DIR = BASE_DIR / "templates"
 
-@app.get("/")
-def home():
-    return {"message": "hello world!"}
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
+
+
+@app.get("/", include_in_schema=False)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
--- src/lingua_loop/main.py
+++ src/lingua_loop/main.py
@@ -4,9 +4,9 @@ import uvicorn
 def run():
     uvicorn.run(
         "lingua_loop.app:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
-        reload=False
+        reload=True
     )
 
 
