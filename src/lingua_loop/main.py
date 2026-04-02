be3e3f8956c71c9601103cb9857383ae0dc1e5da
Jared Frazier <frazier@iap-kborn.de>
2026-04-02 21:50:26 +0200

    setup for testing api

--- src/lingua_loop/main.py
+++ src/lingua_loop/main.py
@@ -22,24 +22,26 @@ async def lifespan(app: FastAPI):
     await session.shutdown()
 
 
-app = FastAPI(lifespan=lifespan)
+def create_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan)
+
+    @app.exception_handler(TranscriptNotFoundError)
+    async def transcript_not_found_handler(
+        request: Request, exc: TranscriptNotFoundError
+    ):
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND, content={"detail": str(exc)}
+        )
+
+    app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+    @app.get("/", include_in_schema=False)
+    def index():
+        return FileResponse(Path(f"{TEMPLATES_DIR}/index.html"))
+
+    app.include_router(transcript.router)
+
+    return app
 
 
-@app.exception_handler(TranscriptNotFoundError)
-async def transcript_not_found_handler(
-    request: Request, exc: TranscriptNotFoundError
-):
-    return JSONResponse(
-        status_code=status.HTTP_404_NOT_FOUND, content={"detail": str(exc)}
-    )
-
-
-app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
-
-
-@app.get("/", include_in_schema=False)
-def index():
-    return FileResponse(Path(f"{TEMPLATES_DIR}/index.html"))
-
-
-app.include_router(transcript.router)
+app = create_app()
--- tests/unit/api/test_api.py
+++ tests/unit/api/test_api.py
@@ -1,2 +1,53 @@
-def test_api():
-    raise NotImplementedError
+from collections.abc import AsyncGenerator
+
+import pytest
+import pytest_asyncio
+from fastapi import FastAPI
+from httpx import ASGITransport
+from httpx import AsyncClient
+from pytest_mock import MockerFixture
+
+from lingua_loop.db.session import get_async_session
+from lingua_loop.main import app as lingua_loop_app
+
+
+@pytest_asyncio.fixture
+async def app(mocker: MockerFixture) -> AsyncGenerator[FastAPI]:
+    async def override_get_async_session():
+        yield mocker.AsyncMock()
+
+    lingua_loop_app.dependency_overrides[get_async_session] = (
+        override_get_async_session
+    )
+    yield lingua_loop_app
+    lingua_loop_app.dependency_overrides.pop(get_async_session)
+
+
+@pytest_asyncio.fixture
+async def client(app: FastAPI):
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as _client:
+        yield _client
+
+
+@pytest.mark.asyncio
+async def test_get_transcript_success(client, mocker: MockerFixture):
+    raise
+
+
+@pytest.mark.asyncio
+async def test_get_transcript_invalid_video_id(client, mocker: MockerFixture):
+    raise
+
+
+@pytest.mark.asyncio
+async def test_score_transcription_success(client, mocker: MockerFixture):
+    raise
+
+
+@pytest.mark.asyncio
+async def test_score_transcription_invalid_segment_ixs(
+    client, mocker: MockerFixture
+):
+    raise
