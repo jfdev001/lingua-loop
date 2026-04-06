65e669ddf2284452849ff6e7dafe39c68f1ff213
Jared Frazier <frazier@iap-kborn.de>
2026-04-06 09:37:12 +0200

    fragile server start

--- src/lingua_loop/scripts/run.py
+++ src/lingua_loop/scripts/run.py
@@ -1,8 +1,20 @@
+import threading
+import webbrowser
+
 import uvicorn
 
 
 def run():
-    uvicorn.run("lingua_loop.main:app", host="0.0.0.0", port=8000, reload=True)
+    """
+    TODO: Starting on port is fragile. Dynamically assign like in jupyter-server
+    or otherwise.
+
+    Reference: https://github.com/Kludex/uvicorn/issues/761#issuecomment-1287679527
+    """
+    host = "127.0.0.1"
+    free_port = 49152
+    threading.Timer(2.0, lambda: webbrowser.open(f"{host}:{free_port}")).start()
+    uvicorn.run("lingua_loop.main:app", host=host, port=free_port, reload=True)
 
 
 if __name__ == "__main__":
